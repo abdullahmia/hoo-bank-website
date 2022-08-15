f05e9a92daa518a8be1e91043e980e77f4782be6
Abdullah Mia <abdullah149161@bsdi-bd.org>
2022-08-15 16:12:56 +0600

    [Billing section]

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -10,7 +10,7 @@ const Business = () => {
         <h2 className={styles.heading2}>You do the business, <br className='sm:block hidden' /> we’ll handle the money.</h2>
         <p className={`${styles.paragraph} max-w-[470px] mt-5`}>With the right credit card, you can improve your financial life by building credit, earning rewards and saving money. But with hundreds of credit cards on the market.</p>
 
-        <Button styles="mt-10" />
+        <Button styles="mt-10 rounded-xl" />
       </div>
 
       <div className={`${layout.sectionImg} flex-col`}>
