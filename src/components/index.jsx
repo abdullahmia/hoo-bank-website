4477a7f78d2ff114537749cbcb729f874ebee222
Abdullah Mia <abdullah149161@bsdi-bd.org>
2022-08-15 13:30:11 +0600

    [Initial Commit]

---
+++ src/components/index.jsx
@@ -0,0 +1,24 @@
+
+import Billing from "./Billing";
+import Business from "./Business";
+import CardDeal from "./CardDeal";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import Footer from "./Footer";
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import Stats from "./Stats";
+import Testimonials from "./Testimonials";
+
+export {
+    Navbar,
+    Billing,
+    CardDeal,
+    Business,
+    Clients,
+    CTA,
+    Stats,
+    Footer,
+    Testimonials,
+    Hero,
+};
