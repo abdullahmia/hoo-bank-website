01476e9ed9af4937e83548e490b62e2e6d104dcb
Abdullah Mia <abdullah149161@bsdi-bd.org>
2022-08-15 16:01:53 +0600

    [Business section]

---
+++ src/components/FeaturedCard.jsx
@@ -0,0 +1,18 @@
+import { features } from "../constants"
+import styles from "../style"
+
+const FeaturedCard = ({icon, title, content, index}) => {
+  return (
+    <div className={`flex flex-row p-6 rounded-[20px] ${index !== features.length - 1 ? "mb-6" : "mb-0" } feature-card`}>
+        <div className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter}`}>
+            <img src={icon} alt="icon" className="w-[50%] h-[50%] object-contain" />
+        </div>
+        <div className="flex-1 flex flex-col ml-3">
+            <h4 className="font-poppins font-semibold text-white text-[18px] leading-[23px] mb-1">{title}</h4>
+            <p className="font-poppins font-normal text-dimWhite text-[16px] leading-[24px] mb-1">{content}</p>
+        </div>
+    </div>
+  )
+}
+
+export default FeaturedCard
