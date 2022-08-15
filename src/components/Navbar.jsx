bfcea28e265ae4ef64a918b5673ce7308112e5af
Abdullah Mia <abdullah149161@bsdi-bd.org>
2022-08-15 13:50:22 +0600

    [Header section]

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import { useState } from 'react';
+import { close, logo, menu } from '../assets';
+import { navLinks } from '../constants';
 
 const Navbar = () => {
+  const [toggole, setToggole] = useState(false);
   return (
-    <div>Navbar</div>
+    <nav className='w-full flex py-6 between items-center navbar'>
+      <img src={logo} alt="HooBank" className='w-[124px] h-[32px]' />
+
+      <ul className='list-none sm:flex hidden justify-end items-center flex-1'>
+        {
+          navLinks.map((nav, key) => (
+            <li key={key} className={`font-poppins font-normal cursor-pointer text-[16px] ${key === navLinks.length ? 'mr-0' : 'mr-10'} text-white`}>
+              <a href={`#${nav.id}`}>{nav.title}</a>
+            </li>
+          ))
+        }
+      </ul>
+
+      <div className='sm:hidden flex flex-1 justify-end items-center '>
+        <img src={toggole ? close : menu} alt="menu" className='w-[28px] h-[28px] object-contain'
+        onClick={() => setToggole((prev) => !prev)}
+        />
+
+        <div className={`${toggole ? 'flex' : 'hidden'} p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}>
+          <ul className='list-none flex flex-col justify-end items-center flex-1'>
+            {
+              navLinks.map((nav, key) => (
+                <li key={key} className={`font-poppins font-normal cursor-pointer text-[16px] ${key === navLinks.length ? 'mb-0' : 'mb-4'} text-white`}>
+                  <a href={`#${nav.id}`}>{nav.title}</a>
+                </li>
+              ))
+            }
+          </ul>
+        </div>
+
+      </div>
+    </nav>
   )
 }
 
-export default Navbar
+export default Navbar;
